a6ab422f639eac6b6f31aa9d59c97f573cfbbf8f
redieight <redietalx@gmail.com>
2021-11-16 20:09:02 +0300

    da

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if (i == 10) or (i == 101):
-            continue
-    print("{:c}".format(i),end="");
-    
+    if (i == 101) or (i == 113):
+        continue
+    print("{:c}".format(i), end="")
