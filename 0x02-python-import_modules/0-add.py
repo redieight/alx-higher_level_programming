ccc37a92e8dfa40227ea3bdec6f460d6c54a6b13
redieight <redietalx@gmail.com>
2021-11-18 19:37:12 +0300

    origin

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import add_0
-a=1
-b=2
-print("{:d} + {:d} = {:d}".format(a, b, add_0.add(1, 2)))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add_0.add_0(1, 2)))
+
