3b010d00aeb49025728161bed4663ec3738e0e5e
redieight <redietalx@gmail.com>
2021-11-18 19:59:40 +0300

    ha

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import add_0
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add_0.add_0(1, 2)))
+    print("{:d} + {:d} = {:d}".format(a, b, add_0.add(1, 2)))
 
