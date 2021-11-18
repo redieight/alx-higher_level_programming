2820d36e81d4d8c693683ef2995d706818a0439e
redieight <redietalx@gmail.com>
2021-11-18 22:47:11 +0300

    ma

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
+import sys
+
+
+def main():
+    l = len(sys.argv)
+    print('{:d} argument{:}'.format(l - 1, '.' if l == 1 else
+                                    (':' if l == 2 else 's:')))
+    i = 1
+    for arg in sys.argv[1:]:
+        print("{:d}: {}".format(i, arg))
+        i += 1
+
 if __name__ == "__main__":
-    import sys
-    argv = sys.argv[1:]
-    argv_count = len(argv)
-    index = 1
-    if argv_count is 0:
-        print("{:d} arguments.".format(argv_count))
-    elif argv_count is 1:
-        print("{:d} argument:".format(argv_count))
-        print("{:d}: {:s}".format(index, sys.argv[1]))
-    else:
-        print("{:d} arguments:".format(argv_count))
-        while index <= argv_count:
-            print("{:d}: {:s}".format(index, sys.argv[index]))
-            index += 1
+    main()
