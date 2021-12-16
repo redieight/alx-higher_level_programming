3985d2acbfd43a94a29e67f358fafbb93d97671e
redieight <redietalx@gmail.com>
2021-12-16 15:24:48 +0300

    nai

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-# 0-add_integer.py
-"""Defines an integer addition function."""
+# 3-say_my_name.py
+"""Defines a name-printing function."""
 
 
-def add_integer(a, b=98):
-    """Return the integer addition of a and b.
+def say_my_name(first_name, last_name=""):
+    """Print a name.
 
-    Float arguments are typecasted to ints before addition is performed.
-
+    Args:
+        first_name (str): The first name to print.
+        last_name (str): The last name to print.
     Raises:
-        TypeError: If either of a or b is a non-integer and non-float.
+        TypeError: If either of first_name or last_name are not strings.
     """
-    if ((not isinstance(a, int) and not isinstance(a, float))):
-        raise TypeError("a must be an integer")
-    if ((not isinstance(b, int) and not isinstance(b, float))):
-        raise TypeError("b must be an integer")
-    return (int(a) + int(b))
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
